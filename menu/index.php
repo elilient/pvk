cad45d937be2785281b373864ab7a0d2c395b12f
edgar.lilienthal <edgar.lilienthal@khk.ee>
2019-02-14 09:58:38 +0200

    Menu_oop course_card_data ja list

--- menu/index.php
+++ menu/index.php
@@ -13,6 +13,7 @@ $mainTmpl->set('page title', 'Tartu KHK Menu');
 $contentTmpl = new Template('content');
 $courseCardTmpl = new Template('course_card');
 $courseCardHeaderTmpl = new Template('course_card_header');
+$courseCardDataTmpl = new Template('course_card_data');
 
 $courseNames = array(
     'praed' => 'fa-utensils', 'supid' => 'fa-utensil-spoon', 'magustoidud' => 'fa-cookie-bite', 'joogid' => 'fa-glass-whiskey'
@@ -22,6 +23,8 @@ foreach ($courseNames as $courseName => $courseIcon){
     $courseCardHeaderTmpl->set('course_name', $courseName);
     $courseCardHeaderTmpl->set('icon', $courseIcon);
     $courseCardTmpl->set('course_card_header', $courseCardHeaderTmpl->parse());
+    $courseCardDataTmpl->set('course_name', $courseName);
+    $courseCardTmpl->set('course_card_data', $courseCardDataTmpl->parse());
     $contentTmpl->add('course_cards', $courseCardTmpl->parse());
 }
 
