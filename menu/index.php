86df07755266108af365897cc5f56bdc35b89fc8
edgar.lilienthal <edgar.lilienthal@khk.ee>
2019-02-14 09:19:55 +0200

    Menu_oop lisatud lehe sisu

--- menu/index.php
+++ menu/index.php
@@ -9,6 +9,12 @@ $mainTmpl = new Template('mail');
 
 $mainTmpl->set('title', 'Menu Application');
 $mainTmpl->set('page title', 'Tartu KHK Menu');
+
+$contentTmpl = new Template('content');
+$mainTmpl->set('content', $contentTmpl->parse());
+
+
+
 $mainTmplContent = $mainTmpl->parse();
 
 echo $mainTmplContent;
