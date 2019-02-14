f4c7cd3e97db9b4d4f0ee9778c6538a6236806aa
edgar.lilienthal <edgar.lilienthal@khk.ee>
2019-02-14 09:52:44 +0200

    Menu_oop ikoonid ja course-d foreach tsüklis

--- menu/index.php
+++ menu/index.php
@@ -13,11 +13,18 @@ $mainTmpl->set('page title', 'Tartu KHK Menu');
 $contentTmpl = new Template('content');
 $courseCardTmpl = new Template('course_card');
 $courseCardHeaderTmpl = new Template('course_card_header');
-$courseCardTmpl->set('course_card_header', $courseCardHeaderTmpl->parse());
-$contentTmpl->add('course_cards', $courseCardTmpl->parse());
-$contentTmpl->add('course_cards', $courseCardTmpl->parse());
-$contentTmpl->add('course_cards', $courseCardTmpl->parse());
-$contentTmpl->add('course_cards', $courseCardTmpl->parse());
+
+$courseNames = array(
+    'praed' => 'fa-utensils', 'supid' => 'fa-utensil-spoon', 'magustoidud' => 'fa-cookie-bite', 'joogid' => 'fa-glass-whiskey'
+);
+
+foreach ($courseNames as $courseName => $courseIcon){
+    $courseCardHeaderTmpl->set('course_name', $courseName);
+    $courseCardHeaderTmpl->set('icon', $courseIcon);
+    $courseCardTmpl->set('course_card_header', $courseCardHeaderTmpl->parse());
+    $contentTmpl->add('course_cards', $courseCardTmpl->parse());
+}
+
 
 $mainTmpl->set('content', $contentTmpl->parse());
 
